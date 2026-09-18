8ef4da2ccc338d1de51ad0e395bdfd457caa995c
pate1382 <pate1382@algonquinlive.com>
2026-09-17 20:03:34 -0400

    Updates

--- assets/waitForFonts-COBeR9Y6.js
+++ assets/waitForFonts-COBeR9Y6.js
@@ -1,4 +1,4 @@
-import{j as I,r as St}from"./index-qI1iIjxX.js";import{g as yt,S as xt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as I,r as St}from"./index-Rybv_6Xm.js";import{g as yt,S as xt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
